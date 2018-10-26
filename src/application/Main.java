951f6902a3b74a9a0bb57e67df0b6ee167acad86
Mario <mni0000@alu.ubu.es>
2018-10-26 22:45:19 +0200

    Pequeños cambios
    Color de fondo, titulo de la ventana y tamaño de la letra.

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,8 @@ import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 
@@ -15,11 +17,14 @@ public class Main extends Application {
 			
 			Text helloWorld = new Text("Hello world");
 			BorderPane.setAlignment(helloWorld, Pos.CENTER);
+			helloWorld.setFont(new Font(70));
 			
 			BorderPane root = new BorderPane(helloWorld);
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,1000,400);
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.setFill(Color.AQUAMARINE);
+			primaryStage.setTitle("Esto es un titulo");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
