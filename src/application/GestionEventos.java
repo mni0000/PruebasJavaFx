eb1ad7a611ff3dd201a72dfb520a6041eb5bfdd8
Mario <mni0000@alu.ubu.es>
2018-10-28 00:20:14 +0200

    Mostrar y ocultar imagenes con botones.

--- src/application/GestionEventos.java
+++ src/application/GestionEventos.java
@@ -1,17 +1,17 @@
 package application;
 
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GestionEventos extends Application{
@@ -19,7 +19,10 @@ public class GestionEventos extends Application{
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			//Raiz de la escena
+			Group root = new Group();
 			
+			//Rectangulo coloreado
 			Rectangle rectangulo = new Rectangle();
 			rectangulo.setHeight(100);
 			rectangulo.setWidth(200);
@@ -27,6 +30,16 @@ public class GestionEventos extends Application{
 			rectangulo.setX(200);
 			rectangulo.setY(0);
 			
+			//Imagen de ejemplo junto con su vista
+			Image imagen = new Image(new FileInputStream("sample.jpg"));
+			ImageView vistaImagen = new ImageView(imagen);
+			//vistaImagen.setFitHeight(100);
+			vistaImagen.setFitWidth(200);
+			vistaImagen.setX(200);
+			vistaImagen.setY(80);
+			vistaImagen.setPreserveRatio(true);
+			
+			//Boton para cambiar el rectangulo a color verde
 			Button verde = new Button("Verde");
 			verde.setOnMouseClicked(new EventHandler<MouseEvent>() {
 				@Override
@@ -34,15 +47,50 @@ public class GestionEventos extends Application{
 					rectangulo.setFill(Color.GREEN);
 				};
 			});
-			verde.setLayoutX(200);
-			verde.setLayoutY(200);
+			verde.setLayoutX(200); 
+			verde.setLayoutY(220);
 			
-			Group root = new Group(rectangulo,verde);
+			//Boton para cambiar el rectangulo a color azul
+			Button azul = new Button("Azul");
+			azul.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent e) {
+					rectangulo.setFill(Color.BLUE);
+				};
+			});
+			azul.setLayoutX(350);
+			azul.setLayoutY(220);
+			
+			//Boton para mostrar la imagen
+			Button mostrarImagen = new Button("Mostrar imagen");
+			mostrarImagen.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent e) {
+					if (!root.getChildren().contains(vistaImagen)) {
+						root.getChildren().add(vistaImagen);
+					}
+				};
+			});
+			mostrarImagen.setLayoutX(200);
+			mostrarImagen.setLayoutY(250);
+			
+			//Boton para ocultar la imagen
+			Button ocultarImagen = new Button("Ocultar imagen");
+			ocultarImagen.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent e) {
+					root.getChildren().remove(vistaImagen);
+				};
+			});
+			ocultarImagen.setLayoutX(300);
+			ocultarImagen.setLayoutY(250);
+			
+			//Agregamos los nodos a la raiz e instanciamos la escena
+			root.getChildren().addAll(rectangulo,verde,azul,mostrarImagen,ocultarImagen);
 			Scene scene = new Scene(root,600,300);
 			
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			//scene.setFill(Color.AQUAMARINE);
-			primaryStage.setTitle("Esto es un titulo");
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Gestion de eventos");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
